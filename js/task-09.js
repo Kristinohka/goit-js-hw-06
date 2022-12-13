119eed37f67d369c73ad181c3465cc445d81bab7
Kristinohka <333kristina33374@gmail.com>
2022-12-13 16:00:03 +0200

    fix

--- js/task-09.js
+++ js/task-09.js
@@ -2,15 +2,13 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
-const bodyEl = document.querySelector("body");
 
-const changeColorEl = document.querySelector(".change-color");
+const bodyСolorChange = document.querySelector("body");
+const buttonСolorChange = document.querySelector(".change-color");
+const nameColorChange = document.querySelector(".color");
 
-const colorEl = document.querySelector(".color");
-
-changeColorEl.addEventListener("click", changeBgColorRandom);
-
-function changeBgColorRandom() {
-  bodyEl.style.backgroundColor = `${getRandomHexColor()}`;
-  colorEl.textContent = getRandomHexColor();
-}
+buttonСolorChange.addEventListener("click", () => {
+  let randomHexColor = getRandomHexColor();
+  bodyСolorChange.style.backgroundColor = randomHexColor;
+  nameColorChange.textContent = randomHexColor;
+});
